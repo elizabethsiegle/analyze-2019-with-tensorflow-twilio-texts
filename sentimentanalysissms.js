72891dea1c971b19c94604bb995b970e0da03c12
Lizzie Siegle <esiegle@brynmawr.edu>
2019-12-05 16:11:07 -0800

    idk man idk

--- sentimentanalysissms.js
+++ sentimentanalysissms.js
@@ -1,5 +1,4 @@
 
-require("@tensorflow/tfjs-node");
 const tf = require("@tensorflow/tfjs");
 
 const fetch = require("node-fetch");
@@ -23,31 +22,32 @@ async function setup() {
 
 
 function getSentiment(score) {
-  switch(true) {
-    case(score > 0.66):
+    if(score > 0.66) {
       return `Score of ${score} is Positive`;
-    case (score > 0.4):
+    } 
+    else if(score > 0.4) {
       return `Score of ${score} is Neutral`;
-    default:
+    }
+    else {
       return `Score of ${score} is Negative`;
-  }
+    }
 }
 
 function predict(text, model, metadata) {
   var trimmed = text.replace(/[|&;$%@"<>()+,$|\d]^\s*[·]+\s*\b/g, "").trim().split("\n");
   //not in dictionary, TF returns error
   const inputBuffer = tf.buffer([1, metadata.max_len], "float32");
-  try {
+  console.log(`trimmed ${trimmed}`);
     trimmed.forEach((word, i) => inputBuffer.set(metadata.word_index[word] + metadata.index_from, 0, i));
     const input = inputBuffer.toTensor(); //returns Tensor obj
     const predictOut = model.predict(input);
     let positivity = predictOut.dataSync()[0];
     predictOut.dispose();
-    return positivity;
-  }
-  catch {
-    return 0; //word/item passed in not in dictionary, return 0
-  }
+    console.log(`try ${positivity}`);
+    if(isNaN(positivity)) {
+      return  0;
+    }
+    return positivity.toFixed(3);
   
 }
 
@@ -55,23 +55,18 @@ const getMetaData = async () => {
   const dt = await fetch("https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/metadata.json")
   return dt.json()
 }
-function getAvg(arr) {
-  const total = arr.reduce((partial_sum, a) => partial_sum + a, 0); 
-  console.log("total ", total, " len arr ", arr.length);
-  return total / arr.length;
-}
 
 async function run(text) {
   const u2 = "https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/model.json"
   const model = await tf.loadLayersModel(u2);
   const metadata = await getMetaData();
-  var avg;
-  var posArr = [];
+  var sum = 0.000;
+  
   text.forEach(function (prediction) {
     avg = predict(prediction, model, metadata);
-    posArr.push(avg);
-  });
-  console.log(getSentiment(getAvg(posArr)));
+    sum += parseFloat(avg, 10);
+  })
+  console.log(getSentiment(sum/text.length));
 }
 
 setup().then(function(result) {
