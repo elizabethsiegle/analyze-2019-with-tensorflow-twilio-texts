23b318eec6397f56fec47741f7caec035a5e36eb
Lizzie Siegle <esiegle@brynmawr.edu>
2019-12-06 02:39:20 -0800

    change trimmed regex

--- sentimentanalysissms.js
+++ sentimentanalysissms.js
@@ -7,48 +7,17 @@ client = require("twilio")(
   process.env.TWILIO_AUTH_TOKEN
 );
 
-async function setup() {
+const setup = async () => {
   var texts = [];
    await client.messages
-    .list({
-      dateSentAfter: new Date(Date.UTC(2019, 0, 1, 0, 0, 0)),
-      dateSentBefore: new Date(Date.UTC(2019, 10, 31, 0, 0, 0)),
-      from: "+16507878004",
-      limit: 500
-    })
+     .list({
+       dateSentBefore: new Date(Date.UTC(2019, 11, 31, 0, 0, 0)),
+       dateSentAfter: new Date(Date.UTC(2019, 0, 1, 0, 0, 0)),
+       from: "+16507878004",
+       limit: 500
+     })
      .then(messages => messages.forEach(m => texts.push(m.body)));
   return Array.from(new Set(texts)); //only unique texts
-  }
-
-
-function getSentiment(score) {
-    if(score > 0.66) {
-      return `Score of ${score} is Positive`;
-    } 
-    else if(score > 0.4) {
-      return `Score of ${score} is Neutral`;
-    }
-    else {
-      return `Score of ${score} is Negative`;
-    }
-}
-
-function predict(text, model, metadata) {
-  var trimmed = text.replace(/[|&;$%@"<>()+,$|\d]^\s*[·]+\s*\b/g, "").trim().split("\n");
-  //not in dictionary, TF returns error
-  const inputBuffer = tf.buffer([1, metadata.max_len], "float32");
-  console.log(`trimmed ${trimmed}`);
-    trimmed.forEach((word, i) => inputBuffer.set(metadata.word_index[word] + metadata.index_from, 0, i));
-    const input = inputBuffer.toTensor(); //returns Tensor obj
-    const predictOut = model.predict(input);
-    let positivity = predictOut.dataSync()[0];
-    predictOut.dispose();
-    console.log(`try ${positivity}`);
-    if(isNaN(positivity)) {
-      return  0;
-    }
-    return positivity.toFixed(3);
-  
 }
 
 const getMetaData = async () => {
@@ -56,24 +25,49 @@ const getMetaData = async () => {
   return dt.json()
 }
 
+const predict = (text, model, metadata) => {
+  //tokenize text: remove non-alphanumeric chars besides spaces, apos
+  var trimmed = text.trim().toLowerCase().replace(/(\.|\,|\!)/g, "").split(/\s+/g);
+  //look up word indices
+  const inputBuffer = tf.buffer([1, metadata.max_len], "float32"); //buffer is an array of integers defining output tensor shape, type
+  //fill buffer with trimmed words and their indices
+  trimmed.forEach((word, i) => inputBuffer.set(metadata.word_index[word] + metadata.index_from, 0, i));
+  const input = inputBuffer.toTensor(); //returns Tensor obj
+  const predictOut = model.predict(input);
+  let positivity = predictOut.dataSync()[0]; //output data retrieved
+  predictOut.dispose();
+  //some words aren't in the English dictionary and TFJS doesn't like these words
+  if(isNaN(positivity)) {
+    return  0;
+  }
+  return positivity;
+}
+
 async function run(text) {
-  const u2 = "https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/model.json"
-  const model = await tf.loadLayersModel(u2);
+  //load pretrained model at remote URL
+  const url = `https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/model.json`
+  const model = await tf.loadLayersModel(url); 
   const metadata = await getMetaData();
-  var sum = 0.000;
-  
+  var sum = 0, avg = 0;
   text.forEach(function (prediction) {
     avg = predict(prediction, model, metadata);
     sum += parseFloat(avg, 10);
+    console.log(`text ${prediction} avg ${avg}`);
   })
   console.log(getSentiment(sum/text.length));
 }
+const getSentiment = (score) => {
+  if (score > 0.66) {
+    return `Score of ${score} is Positive`;
+  }
+  else if (score > 0.4) {
+    return `Score of ${score} is Neutral`;
+  }
+  else {
+    return `Score of ${score} is Negative`;
+  }
+}
 
 setup().then(function(result) {
-  var filtered = result
-    .filter(function(el) {
-      return el != "";
-    })
-    .map(v => v.toLowerCase()); 
-  run(filtered);
+  run(result); 
 });
